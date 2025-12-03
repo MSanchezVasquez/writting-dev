77620cdda75f8e6b407253bf87ad4beef95aeb7d
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2025-12-03 16:57:49 -0500

    feat: migrate Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <div>
+        writings.dev
+        <a
+          href="https://twitter.com/mcodex"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="author-link"
+        >
+          by @mcodex
+        </a>
+      </div>
+
+      <button>
+        {/* Asegúrate de que este icono esté en tu carpeta public/assets */}
+        <img
+          src="/assets/twitter_x_new_logo_x_icon_256077.svg"
+          alt="Twitter Logo"
+        />
+        <p>Follow @mcodex23</p>
+      </button>
+    </footer>
+  );
+};
